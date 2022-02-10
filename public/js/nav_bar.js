59ec33eca52af083c2c84bf098bc257772e095bb
Gabriel-mq <gabrielmakedas@gmail.com>
2022-02-10 14:12:18 -0300

    clean code nav_bar

--- public/js/nav_bar.js
+++ public/js/nav_bar.js
@@ -7,15 +7,9 @@ export default function nav_bar(hamburger, header, navBar) {
 
   d.addEventListener('click', (e) => {
     if (e.target.matches(hamburger) || e.target.matches(`${hamburger} *`)) {
-      if ($header.hasAttribute('class') === false) {
-        $header.classList.add('hamburger__retract')
-        $hamburger.classList.toggle('is-active')
-        $navBar.classList.toggle('header__container-opacity')
-      } else {
-        $header.classList.toggle('hamburger__retract')
-        $hamburger.classList.toggle('is-active')
-        $navBar.classList.toggle('header__container-opacity')
-      }
+      $header.classList.toggle('hamburger__retract')
+      $hamburger.classList.toggle('is-active')
+      $navBar.classList.toggle('header__container-opacity')
     }
   })
 }
